b6a70fc8e4359fe237a0b877ef4767c12fd104fc
KlausMichalsky <k.michalsky81@googlemail.com>
2025-12-13 19:01:37 +0100

    homing mejorado

---
+++ Stepper_Homing_2Motores_Hall_AS5600_I2C/build/core/StackThunk.cpp.d
@@ -0,0 +1,3 @@
+c:\Users\Benutzer1\Documents\Arduino\Stepper\Stepper_Homing_2Motores_Hall_AS5600_I2C\build\core\StackThunk.cpp.o: \
+ C:\Users\Benutzer1\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\4.5.2\cores\rp2040\StackThunk.cpp \
+ C:\Users\Benutzer1\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\4.5.2\cores\rp2040\StackThunk.h
---
+++ Stepper_Homing_2Motores_Hall_AS5600_I2C/build/core/stdlib_noniso.cpp.d
@@ -0,0 +1,3 @@
+c:\Users\Benutzer1\Documents\Arduino\Stepper\Stepper_Homing_2Motores_Hall_AS5600_I2C\build\core\stdlib_noniso.cpp.o: \
+ C:\Users\Benutzer1\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\4.5.2\cores\rp2040\stdlib_noniso.cpp \
+ C:\Users\Benutzer1\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\4.5.2\cores\rp2040\stdlib_noniso.h
